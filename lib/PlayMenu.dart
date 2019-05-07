2ac358dad40f5c0ebe78cdaaa6a2a1efa0956446
593361260 <593361260@qq.com>
2019-05-07 19:42:51 +0800

    详情展示列表接口接入, 连续滚动

--- lib/PlayMenu.dart
+++ lib/PlayMenu.dart
@@ -3,13 +3,13 @@ import 'package:flutter/material.dart';
 /*
 * 播放的界面
 * */
-class PlayMenuWidget extends StatefulWidget {
+class play_menu_widget extends StatefulWidget {
   String imgUrl;
   String id;
   String playUrl;
   String info;
 
-  PlayMenuWidget({Key key, this.id, this.imgUrl, this.info, this.playUrl})
+  play_menu_widget({Key key, this.id, this.imgUrl, this.info, this.playUrl})
       : super(key: key);
 
   @override
@@ -18,11 +18,27 @@ class PlayMenuWidget extends StatefulWidget {
   }
 }
 
-class _PlayMenuBuilder extends State<PlayMenuWidget> {
+class _PlayMenuBuilder extends State<play_menu_widget> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: AppBar(),
+      body: Center(
+          child: RotationTransition(
+        child: ClipOval(
+          child: Image(
+            image: NetworkImage(
+                "http://img.ring.51app.cn/20190329/42635586d11843619149799c6f5b8b1a.png"),
+            fit: BoxFit.cover,
+            width: 200,
+            height: 200,
+          ),
+        ),
+        turns: new CurvedAnimation(
+            parent: new AnimationController(
+                duration: const Duration(milliseconds: 2000),
+                vsync: new AnimatedListState()),
+            curve: Curves.easeIn),
+      )),
     );
   }
 }
--- lib/SubjectDetailsPage.dart
+++ lib/SubjectDetailsPage.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_list/PlayMenu.dart';
 
 class SubjectDetailsPage extends StatefulWidget {
   String id;
@@ -36,20 +37,82 @@ class _SubjectDetailsBuilder extends State<SubjectDetailsPage> {
                     fit: BoxFit.fitWidth,
                   ),
                 ),
-                ListView()
+                Flexible(
+                  flex: 1,
+                  fit: FlexFit.tight,
+                  child: ListView(
+                    children: _getItem(),
+                  ),
+                )
               ],
             )
-          : {
-              getDetails(id),
+          : getView(),
+    );
+  }
+
+  List<Widget> _getItem() {
+    List data = body['ringtones'];
+    return data.map((f) {
+      return Card(
+        child: _getItemView(f),
+        margin: EdgeInsets.fromLTRB(2.5, 2.5, 2.5, 2.5),
+        elevation: 2,
+      );
+    }).toList();
+  }
+
+  Widget _getItemView(f) {
+    return new GestureDetector(
+      child: Row(
+        children: <Widget>[
+          Container(
+            width: 44,
+            height: 44,
+            margin: EdgeInsets.fromLTRB(10, 5, 0, 5),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(5),
+                image: DecorationImage(
+                    image: NetworkImage(f['imageUrl']), fit: BoxFit.cover)),
+          ),
+          new Flexible(
+            flex: 1,
+            fit: FlexFit.tight,
+            child: Column(children: <Widget>[
               Container(
-                width: double.infinity,
-                height: 214,
-                child: Image(
-                  image: NetworkImage(imgUrl),
-                  fit: BoxFit.fitWidth,
+                child: Text(
+                  f['name'] + '-' + f['singer'],
+                  style: TextStyle(color: Colors.black, fontSize: 13),
                 ),
-              )
-            },
+                padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
+              ),
+              Container(
+                child: Text(
+                  f['duration'] + '-' + f['size'] + '-' + f['count'],
+                  style: TextStyle(color: Color(0xff777777), fontSize: 11),
+                ),
+                padding: EdgeInsets.fromLTRB(10, 5, 10, 0),
+              ),
+            ], crossAxisAlignment: CrossAxisAlignment.start),
+          )
+        ],
+      ),
+      onTap: () {
+        Navigator.of(context).push(MaterialPageRoute<void>(builder: (context) {
+          return new play_menu_widget(imgUrl: f['imageUrl']);
+        }));
+      },
+    );
+  }
+
+  Widget getView() {
+    getDetails(id);
+    return Container(
+      width: double.infinity,
+      height: 214,
+      child: Image(
+        image: NetworkImage(imgUrl),
+        fit: BoxFit.fitWidth,
+      ),
     );
   }
 
@@ -59,6 +122,7 @@ class _SubjectDetailsBuilder extends State<SubjectDetailsPage> {
         .getUrl(Uri.parse('http://api.ring.51app.cn/r/specialList/$id.do'));
     var response = await request.close();
     var responseBody = await response.transform(Utf8Decoder()).join();
+    print('$response');
     var list = json.decode(responseBody)["body"];
     setState(() {
       body = list;
