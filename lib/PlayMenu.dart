b8b5f62c11dc83c922e36f05bb7ebe0dde83875c
593361260 <593361260@qq.com>
2019-05-13 23:25:52 +0800

    播放界面UI展示

--- lib/PlayMenu.dart
+++ lib/PlayMenu.dart
@@ -30,23 +30,70 @@ class _PlayMenuBuilder extends State<play_menu_widget> {
   Widget build(BuildContext context) {
     player.play(playUrl);
     return new Scaffold(
-      body: Center(
-          child: RotationTransition(
-        child: ClipOval(
-          child: Image(
-            image: NetworkImage(imgUrl),
-            fit: BoxFit.cover,
-            width: 200,
-            height: 200,
+      body: Column(
+        children: <Widget>[
+          Container(
+            child: Center(
+              child: RotationTransition(
+                child: ClipOval(
+                  child: Image(
+                    image: NetworkImage(imgUrl),
+                    fit: BoxFit.cover,
+                    width: 200,
+                    height: 200,
+                  ),
+                ),
+                turns: new CurvedAnimation(
+                  parent: new AnimationController(
+                      duration: const Duration(milliseconds: 2000),
+                      vsync: new AnimatedListState()),
+                  curve: Curves.easeIn,
+                ),
+              ),
+            ),
+            margin: EdgeInsets.fromLTRB(0, 100, 0, 0),
           ),
-        ),
-        turns: new CurvedAnimation(
-          parent: new AnimationController(
-              duration: const Duration(milliseconds: 2000),
-              vsync: new AnimatedListState()),
-          curve: Curves.easeIn,
-        ),
-      )),
+          Padding(
+            child: Row(
+              children: <Widget>[
+                GestureDetector(child:
+                Icon(
+                  Icons.pause,
+                ),
+                onTap: (){
+                  if(player.state==AudioPlayerState.PLAYING){//正在播放
+                    player.pause();
+                  }else{
+                    if(player.state==AudioPlayerState.PAUSED) {
+                      player.resume();
+                    }else if (player.state==AudioPlayerState.COMPLETED){
+                      player.play(playUrl);
+                    }else{
+
+                    }
+                  }
+                },
+                )
+                ,
+                Flexible(
+                  child: Container(
+                    child: Slider(
+                        min: 0, max: 2000, value: 100, onChanged: (value) {
+//                          player.
+                      print('change value $value');
+                    }),
+                  ),
+                  flex: 1,
+                  fit: FlexFit.tight,
+                ),
+                Icon(Icons.play_arrow),
+              ],
+              mainAxisAlignment: MainAxisAlignment.center,
+            ),
+            padding: EdgeInsets.fromLTRB(20, 100, 20, 0),
+          ),
+        ],
+      ),
     );
   }
 
